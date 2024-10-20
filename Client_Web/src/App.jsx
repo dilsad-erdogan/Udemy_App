eae43c0b4735bd20dd3f4496cbf9ed5f9c810360
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-20 22:57:18 +0300

    pages

--- Client_Web/src/App.jsx
+++ Client_Web/src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StartingPage from "./pages/StartingPage";
+import StudentMain from "./pages/Student/Main";
+import StudentVideo from "./pages/Student/Video";
+import TeacherMain from "./pages/Teacher/Main";
+import TeacherVideo from "./pages/Teacher/Video";
+import TeacherLive from "./pages/Teacher/Live";
+import AdminMain from "./pages/Admin/Main";
 
 function App() {
   return (
-    <>
-      Merhaba dünya
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartingPage />}></Route>
+
+        <Route path="/student" element={<StudentMain />}></Route>
+        <Route path="/student/video" element={<StudentVideo />}></Route>
+
+        <Route path="/teacher" element={<TeacherMain />}></Route>
+        <Route path="/teacher/video" element={<TeacherVideo />}></Route>
+        <Route path="/teacher/live" element={<TeacherLive />}></Route>
+
+        <Route path="/admin" element={<AdminMain />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Client_Web/src/pages/StartingPage.jsx
@@ -0,0 +1,8 @@
+
+const StartingPage = () => {
+  return (
+    <div>StartingPage</div>
+  )
+}
+
+export default StartingPage
