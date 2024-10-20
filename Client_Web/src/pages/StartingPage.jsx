5746552c7997cd9745b905298c8078cbafd6ad38
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-21 00:47:35 +0300

    Services section

--- Client_Web/src/pages/StartingPage.jsx
+++ Client_Web/src/pages/StartingPage.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero/StartPageHero"
 import StartPageNavbar from "../components/Navbar/StartPageNavbar"
+import Services from "../components/Services"
 
 const StartingPage = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <StartPageNavbar />
       <Hero />
+      <Services />
     </main>
   )
 }
