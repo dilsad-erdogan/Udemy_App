7567fb3abda93068668c9f9880b5ce0bc5152959
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-21 23:45:43 +0300

    changes

--- Client_Web/src/components/Video/Video.jsx
+++ Client_Web/src/components/Video/Video.jsx
@@ -5,7 +5,7 @@ const Video = ({ HeroData }) => {
     <div className="mx-auto py-12 px-4 md:px-16 lg:px-24">
         <h2 className="text-2xl font-bold mb-6 text-center">Videos</h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 cursor-pointer">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 cursor-pointer">
             {HeroData.map(((data) => (
                 <div key={data._id}>
                     <VideoCart data={data} />
