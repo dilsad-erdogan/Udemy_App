c5041f8ddaa03def87ba986969a0bf96f1884e44
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-25 19:59:25 +0300

    message box

--- Client_Web/src/components/Chat/Message.jsx
+++ Client_Web/src/components/Chat/Message.jsx
@@ -1,13 +1,13 @@
 
 const Message = ({ message }) => {
     return (
-        <div className="flex flex-col items-start gap-2.5 mb-4">
-            <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                <span className="text-sm font-semibold text-gray-500">{message.name}</span>
+        <div className="flex flex-col items-start mb-4">
+            <div className="flex items-center">
+                <span className="text-sm font-semibold text-gray-400">{message.name}</span>
             </div>
             
-            <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-2 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl">
-                <p className="text-sm font-normal py-2.5 text-gray-900">{message.text}</p>
+            <div className="flex flex-col w-full max-w-[320px] pl-2">
+                <p className="text-sm font-normal py-1 text-black">{message.text}</p>
             </div>
         </div>
     )
