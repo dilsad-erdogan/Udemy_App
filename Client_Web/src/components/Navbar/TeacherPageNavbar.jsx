984244c8cdb50192c8899900bbc3a4c0eeb4b5a4
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-25 20:57:40 +0300

    teacher navbar

--- Client_Web/src/components/Navbar/TeacherPageNavbar.jsx
+++ Client_Web/src/components/Navbar/TeacherPageNavbar.jsx
@@ -1,29 +1,32 @@
+import { useNavigate } from "react-router-dom";
 
 const TeacherPageNavbar = () => {
-  return (
-    <nav>
-        <div className="container py-8 flex justify-between items-center">
-            {/* Logo Section */}
-            <div>
-                <h1 className="font-bold text-2xl">UDEMY | DLSD</h1>
-            </div>
+    const navigate = useNavigate();
 
-            {/* Pages Section */}
-            <div className="flex">
-                <a href="/teacher/video" className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
-                    <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
-                    Add Video
-                </a>
-                <a href="/teacher/live" className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
-                    <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
-                    Add Live
-                </a>
-            </div>
+    return (
+        <nav>
+            <div className="container py-8 flex justify-between items-center">
+                {/* Logo Section */}
+                <div>
+                    <h1 className="font-bold text-2xl" onClick={() => navigate('/teacher')}>UDEMY | DLSD</h1>
+                </div>
 
-            <button className="primary-btn">LogOut</button>
-        </div>
-    </nav>
-  )
+                {/* Pages Section */}
+                <div className="flex">
+                    <a href="/teacher/video" className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
+                        <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
+                        Add Video
+                    </a>
+                    <a href="/teacher/live" className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
+                        <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
+                        Add Live
+                    </a>
+                </div>
+
+                <button className="primary-btn">LogOut</button>
+            </div>
+        </nav>
+    )
 }
 
 export default TeacherPageNavbar
