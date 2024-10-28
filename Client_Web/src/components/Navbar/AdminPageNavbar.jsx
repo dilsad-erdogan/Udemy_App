976582e8bd7a7f08cb405dc4be0a930daa774180
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-28 18:55:11 +0300

    update admin navbar

--- Client_Web/src/components/Navbar/AdminPageNavbar.jsx
+++ Client_Web/src/components/Navbar/AdminPageNavbar.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const AdminPageNavbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav>
         <div className="container py-8 flex justify-between items-center">
             {/* Logo Section */}
             <div>
-                <h1 className="font-bold text-2xl">UDEMY | DLSD</h1>
+                <h1 className="font-bold text-2xl" onClick={() => navigate('/admin')}>UDEMY | DLSD</h1>
             </div>
 
             <button className="primary-btn">LogOut</button>
