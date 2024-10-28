e88365e6a4aec4282f7ff414bd28f8c3852a3097
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-28 18:15:17 +0300

    update student 3

--- Client_Web/src/components/Navbar/StudentPageNavbar.jsx
+++ Client_Web/src/components/Navbar/StudentPageNavbar.jsx
@@ -23,10 +23,10 @@ const StudentPageNavbar = () => {
             </div>
 
             <div className="flex text-center justify-center items-center gap-2 mr-3">
-                <a href="/student/video" className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
+                <div onClick={() => navigate('/student/video')} className="inline-block py-2 px-3 hover:text-secondary relative group font-bold">
                     <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
                     My Videos
-                </a>
+                </div>
 
                 <Link to="/student/cart" className="relative">
                     <FaShoppingCart className="hover:text-secondary relative font-bold" />
