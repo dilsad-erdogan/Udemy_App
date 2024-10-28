088f35f3e03601aef12a1381db2d78fd2a8de933
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-28 15:44:06 +0300

    starting page

--- Client_Web/src/components/Navbar/StartPageNavbar.jsx
+++ Client_Web/src/components/Navbar/StartPageNavbar.jsx
@@ -1,36 +1,15 @@
 import { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
+import { Link } from "react-scroll";
 import Modal from "../Modal";
 import Login from "../userOperations/Login";
 import Register from "../userOperations/Register";
 import FALogin from "../userOperations/FALogin";
 
 const NavbarMenu = [
-    {
-        id: 1,
-        title: "Home",
-        path: "/"
-    },
-    {
-        id: 2,
-        title: "Services",
-        path: "#"
-    },
-    {
-        id: 3,
-        title: "About Me",
-        path: "#"
-    },
-    {
-        id: 4,
-        title: "My Team",
-        path: "#"
-    },
-    {
-        id: 5,
-        title: "Contact Me",
-        path: "#"
-    },
+    { id: 1, title: "Home", path: "home" },
+    { id: 2, title: "Services", path: "services" },
+    { id: 5, title: "Contact Me", path: "contact" },
 ];
 
 const StartPageNavbar = () => {
@@ -55,58 +34,47 @@ const StartPageNavbar = () => {
 
   return (
     <nav>
-        <div className="container py-8 flex justify-between items-center">
-            {/* Logo Section */}
-            <div>
-                <h1 className="font-bold text-2xl">UDEMY | DLSD</h1>
-            </div>
-
-            {/* Menu Section */}
-            <div className="hidden lg:block">
-                <ul className="flex gap-3 items-center">
-                    {NavbarMenu.map((menu) => (
-                        <li key={menu.id}>
-                            <a href={menu.path} className="inline-block py-2 px-3 hover:text-secondary relative group">
-                                <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
-                                {menu.title}
-                            </a>
-                        </li>
-                    ))}
-
-                    <button className="primary-btn" onClick={() => setIsModalOpen(true)}>Login</button>
-                </ul>
-            </div>
-
-            {/* Mobile Menu Section */}
-            <div className="lg:hidden">
-                <IoMdMenu className="text-4xl" onClick={() => setMenuOpen(!menuOpen)} />
-            </div>
-            
-            {menuOpen && (
-                <div className="lg:hidden mt-4">
-                    <ul className="flex flex-col items-start gap-2">
-                        {NavbarMenu.map((menu) => (
-                            <li key={menu.id}>
-                                <a href={menu.path} className="inline-block py-2 px-3 hover:text-secondary relative group">
-                                    <div className="w-2 h-2 absolute bg-secondary mt-2 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
-                                    {menu.title}
-                                </a>
-                            </li>
-                        ))}
-
-                        <button className="primary-btn" onClick={() => setIsModalOpen(true)}>Login</button>
-                    </ul>                        
-                </div>
-            )}
+      <div className="container py-8 flex justify-between items-center">
+        <div>
+          <h1 className="font-bold text-2xl">UDEMY | DLSD</h1>
         </div>
-
-        <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
-            { isLogin === "login" && <Login openFALogin={openFALogin} openRegister={openRegister} /> }
-            { isLogin === "register" && <Register openLogin={openLogin} /> }
-            { isLogin === "falogin" && <FALogin openLogin={openLogin} openRegister={openRegister} /> }
-        </Modal>
+        <div className="hidden lg:block">
+          <ul className="flex gap-3 items-center">
+            {NavbarMenu.map((menu) => (
+              <li key={menu.id}>
+                <Link to={menu.path} smooth={true} duration={500} className="inline-block py-2 px-3 cursor-pointer hover:text-secondary relative group" >
+                  {menu.title}
+                </Link>
+              </li>
+            ))}
+            <button className="primary-btn" onClick={() => setIsModalOpen(true)}>Login</button>
+          </ul>
+        </div>
+        <div className="lg:hidden">
+          <IoMdMenu className="text-4xl" onClick={() => setMenuOpen(!menuOpen)} />
+        </div>
+        {menuOpen && (
+          <div className="lg:hidden mt-4">
+            <ul className="flex flex-col items-start gap-2">
+              {NavbarMenu.map((menu) => (
+                <li key={menu.id}>
+                  <Link to={menu.path} smooth={true} duration={500} className="inline-block py-2 px-3 cursor-pointer hover:text-secondary relative group" >
+                    {menu.title}
+                  </Link>
+                </li>
+              ))}
+              <button className="primary-btn" onClick={() => setIsModalOpen(true)}>Login</button>
+            </ul>                        
+          </div>
+        )}
+      </div>
+      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+        { isLogin === "login" && <Login openFALogin={openFALogin} openRegister={openRegister} /> }
+        { isLogin === "register" && <Register openLogin={openLogin} /> }
+        { isLogin === "falogin" && <FALogin openLogin={openLogin} openRegister={openRegister} /> }
+      </Modal>
     </nav>
-  )
-}
+  );
+};
 
-export default StartPageNavbar
+export default StartPageNavbar;
--- Client_Web/src/pages/StartingPage.jsx
+++ Client_Web/src/pages/StartingPage.jsx
@@ -1,17 +1,17 @@
-import Banner from "../components/Banner"
-import Hero from "../components/Hero/StartPageHero"
-import StartPageNavbar from "../components/Navbar/StartPageNavbar"
-import Services from "../components/Services"
+import Banner from "../components/Banner";
+import Hero from "../components/Hero/StartPageHero";
+import StartPageNavbar from "../components/Navbar/StartPageNavbar";
+import Services from "../components/Services";
 
 const StartingPage = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <StartPageNavbar />
-      <Hero />
-      <Services />
-      <Banner />
+      <section id="home"><Hero /></section>
+      <section id="services"><Services /></section>
+      <section id="contact"><Banner /></section>
     </main>
-  )
-}
+  );
+};
 
-export default StartingPage
+export default StartingPage;
