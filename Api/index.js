d15b14e60ed2e7073a2415eb34989e9c1758720e
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-10-30 00:11:51 +0300

    index.js

--- Api/index.js
+++ Api/index.js
@@ -13,6 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+app.use('/auth', require('./routers/auth'));
+app.use('/live', require('./routers/live'));
+app.use('/permission', require('./routers/permission'));
+app.use('/purchase', require('./routers/purchase'));
+app.use('/role', require('./routers/role'));
+app.use('/user', require('./routers/user'));
+app.use('/video', require('./routers/video'));
+
 app.get('/', (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.send("Homepage");
