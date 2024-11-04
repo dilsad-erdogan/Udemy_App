b460ba0068ed7c8e323e10c820d4a2e0dccfa656
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-11-04 23:38:09 +0300

    modal and hero

--- Client_Web/src/components/Cart/ExplanationCard.jsx
+++ Client_Web/src/components/Cart/ExplanationCard.jsx
@@ -1,12 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { fetchById } from "../../redux/userSlice";
 
 const ExplanationCard = ({ data }) => {
+    const dispatch = useDispatch();
+    const users = useSelector(state => state.user.users);
+
+    useEffect(() => {
+        if (data.teacher_id) {
+            dispatch(fetchById(data.teacher_id));
+        }
+    }, [dispatch, data.teacher_id]);
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left">
                 <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
-                <p className="text-xl sm:text-3xl lg:text-xl font-bold text-right">{data.teacher_id}</p>
+                <p className="text-xl sm:text-3xl lg:text-xl font-bold text-right">
+                    {users?.data?.name || "Loading..."}
+                </p>
                 <p className="text-xl sm:text-3xl lg:text-xl font-bold break-words">{data.description}</p>
             </div>
 
@@ -17,7 +30,7 @@ const ExplanationCard = ({ data }) => {
                 </Link>
             </div>
         </div> 
-    )
-}
+    );
+};
 
-export default ExplanationCard
+export default ExplanationCard;
--- Client_Web/src/components/Cart/ModalCard.jsx
+++ Client_Web/src/components/Cart/ModalCard.jsx
@@ -1,8 +1,11 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../redux/cartSlice"
+import { useEffect } from "react";
+import { fetchById } from "../../redux/userSlice";
 
 const ModalCard = ({ data }) => {
     const dispatch = useDispatch();
+    const users = useSelector(state => state.user.users);
 
     const handleAddToCart = (e, data) => {
         e.stopPropagation()
@@ -11,11 +14,19 @@ const ModalCard = ({ data }) => {
         alert('Video Added Succesfully!');
     };
 
+    useEffect(() => {
+        if (data.teacher_id) {
+            dispatch(fetchById(data.teacher_id));
+        }
+    }, [dispatch, data.teacher_id]);
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left">
                 <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
-                <p className="text-xl sm:text-3xl lg:text-xl font-bold text-right">{data.teacher_id}</p>
+                <p className="text-xl sm:text-3xl lg:text-xl font-bold text-right">
+                    {users?.data?.name || "Loading..."}
+                </p>
                 <p className="text-xl sm:text-3xl lg:text-xl font-bold text-right">${data.price}</p>
                 <p className="text-xl sm:text-3xl lg:text-xl font-bold break-words">{data.description}</p>
             </div>
