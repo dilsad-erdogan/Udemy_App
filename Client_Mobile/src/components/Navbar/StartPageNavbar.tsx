c71f65b02ed3811e64b3b54161ec24bab344d607
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-11-22 00:05:11 +0300

    startpage navbar design

--- Client_Mobile/src/components/Navbar/StartPageNavbar.tsx
+++ Client_Mobile/src/components/Navbar/StartPageNavbar.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { Modal, Text, TouchableOpacity, View } from 'react-native'
+import Login from '../userOperations/Login';
+import Register from '../userOperations/Register';
+import FaLogin from '../userOperations/FaLogin';
 
 const StartPageNavbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState("login");
+
+  const openRegister = () => {
+    setIsLogin("register");
+    setIsModalOpen(true);
+  };
+
+  const openLogin = () => {
+    setIsLogin("login");
+    setIsModalOpen(true);
+  };
+
+  const openFALogin = () => {
+    setIsLogin("falogin");
+    setIsModalOpen(true);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
   return (
-    <Text>StartPageNavbar</Text>
+    <View className='flex flex-row justify-between items-center p-16'>
+      <Text className='font-bold text-2xl'>UDEMY | DLSD</Text>
+
+      <TouchableOpacity className='inline-block bg-primary font-semibold rounded-lg duration-200' onPress={openModal}>
+        <Text className='text-white py-2 px-6'>Login</Text>
+      </TouchableOpacity>
+
+      <Modal visible={isModalOpen} animationType='slide' transparent={true}>
+        <View className='flex flex-1 justify-center items-center bg-gray-300 p-10'>
+          { isLogin === "login" && <Login /> }
+          { isLogin === "register" && <Register /> }
+          { isLogin === "falogin" && <FaLogin /> }
+
+          <TouchableOpacity className='mt-5 p-2 bg-gray-700' onPress={() => setIsModalOpen(false)}>
+            <Text className='text-white text-2xl'>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </View>
   )
 }
 
